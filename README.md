3d5f1917fa51b470880f69ae12f6903489f4deb0
Mactavish <maczhanchao@yahoo.com>
2018-04-02 16:24:38 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue CSS Module Demo
+
+This is a simple demo project for using css modules in Vue components
+
+## Run the example
+
+recommend using yarn over npm:
+
+``` bash
+$ yarn install
+$ yarn serve
+```
+
+## Note
+
+This project is initialized by @vue/cli, explore more configurations on its [documentation](https://github.com/vuejs/vue-cli/blob/dev/docs/README.md)
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="$style.app">
+    <img src="./assets/img/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  created() {
+    console.log(this.$style)
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import './assets/sass/App.module.scss';
+</style>
